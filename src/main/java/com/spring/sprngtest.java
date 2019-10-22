64b495a23954e751e606dc0c623b0ced0a961da3
SaltedFishxy <63410736@qq.com>
2019-10-22 16:43:28 +0800

    rff

--- src/main/java/com/spring/sprngtest.java
+++ src/main/java/com/spring/sprngtest.java
@@ -6,7 +6,7 @@ public class sprngtest {
         System.out.println("123");
         System.out.println("1234");
         System.out.println("1235");
-        System.out.println("1236");
+        System.out.println("12336");
         System.out.println("1237");
     }
 }
