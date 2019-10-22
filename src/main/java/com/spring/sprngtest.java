800879fffc91d92797dcf1b4c5895a6cb87234c8
SaltedFishxy <63410736@qq.com>
2019-10-22 16:08:50 +0800

    123

---
+++ src/main/java/com/spring/sprngtest.java
@@ -0,0 +1,12 @@
+package com.spring;
+
+public class sprngtest {
+    public static void main(String[] args) {
+
+        System.out.println("123");
+        System.out.println("1234");
+        System.out.println("1235");
+        System.out.println("1236");
+        System.out.println("1237");
+    }
+}
