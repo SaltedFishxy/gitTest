c70c4ce9c64b3dd3183362f010ebb1c6918f9cab
SaltedFishxy <63410736@qq.com>
2019-10-30 17:05:46 +0800

    123

--- src/main/java/com/spring/sprngtest.java
+++ src/main/java/com/spring/sprngtest.java
@@ -1,8 +1,9 @@
 package com.spring;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class sprngtest {
     public static void main(String[] args) {
-
         System.out.println("123");
         System.out.println("1234");
         System.out.println("1235");
